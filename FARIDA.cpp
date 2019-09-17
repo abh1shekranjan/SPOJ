f6db716f7d110484a40c89d72171c0cf0295dd5f
Abhishek Ranjan <ranjan.abhishek@protonmail.com>
2019-09-17 06:03:44 +0530

    Accepted SOlution

--- FARIDA.cpp
+++ FARIDA.cpp
@@ -1,59 +1,39 @@
-#include <iostream>
-#include<vector>
+#include<cstdio>
 #include<algorithm>
 
-using namespace std;
+typedef long long int_64 ;
 
-long long int  max(long long int a,long long int b){
-	return a>b?a:b;
-}
-long long int maxGold(long long int *mGold,int n){
-    if (n == 0)
-        return 0;
-    if (n == 1)
-        return mGold[0];
-    if (n==2)
-        return max(mGold[0],mGold[1]);
-    long long int dp[n+9]={0};
-    dp[0]=mGold[0];
-    dp[1]=max(mGold[0],mGold[1]);
-    for (int i = 2; i < n; i++)
-    {
-        dp[i]=max(mGold[i]+dp[i-2],mGold[i-1]);
-    }
-    long long int max=0;
-    for(int i=0;i<=n;i++){
-    	if(max<dp[i]){
-    		max=dp[i];
-    	}
-    }
-    return max;
-    
-    
-}
+using namespace std ; 
 
-int main(){
-    int t,N;
-    cin>>t;
-    int count=1;
-    while (t--)
-    {
-
-        cin>>N;
-        if(N==0){
-        	cout<<"Case 1: 0"<<endl;
-        }
-        else{
-        	 long long int monsterGold[10009]={0};
-        for (int i = 0; i < N; i++)
-        {
-            long long int temp;
-            cin>>temp;
-            monsterGold[i]=temp;
-        }
-        long long int answer=maxGold(monsterGold,N);
-        cout<<"Case "<<count++<<": "<<answer<<endl;
-        }
-    }
-    
+int main( )
+{
+  	int n , i , t , c ;
+	
+	scanf("%d",&t) ;
+	
+	for( c = 1 ; c <= t ; c++ )
+	{
+		scanf("%d",&n) ;
+		
+		if( n == 0 )
+		{
+			printf("Case %d: 0\n",c) ;
+			continue ;
+		}
+		
+		int_64 a[n] , dp[n] ;
+	
+		for( i = 0 ; i < n ; i++ )
+			scanf("%lld",&a[i]) ;
+	
+		dp[0] = a[0] ;
+		dp[1] = max( a[0] , a[1] ) ;
+	
+		for( i = 2 ; i < n ; i++ )
+			dp[i] = max( a[i] + dp[i-2] , dp[i-1] ) ;
+		
+		printf("Case %d: %lld\n",c,dp[n-1]) ;
+	}
+	
+	return 0 ;
 }
