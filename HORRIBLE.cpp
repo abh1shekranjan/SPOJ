0ac58dc4cae17b3b915b51600cf8e3311aec047f
Abhishek Ranjan <ranjan.abhishek@protonmail.com>
2019-09-27 03:44:05 +0530

    Segment Trees and Fast io

--- HORRIBLE.cpp
+++ HORRIBLE.cpp
@@ -1,57 +1,108 @@
-#include<iostream>
-#include<algorithm>
-int n;
-int tree[4000001]={0};
-int lazy[4000001]={0};
-void build(int node,int start,int end){
-    if (start == end)
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int N=100005;
+long long int lazy[4*N];
+long long int seg[4*N];
+ 
+void build(int low,int high,int node)
+{
+    if(low>high)
+    return;
+    if(low == high)
     {
-        tree[node]=arr[start];
+        seg[node]=0;
+        return;
     }
-    else
-    {
-        int mid=(start+end)/2;
-        build(2*node,start,mid);
-        build(2*node+1,mid+1,end);
-        tree[node]=tree[2*node]+tree[2*node+1];
-    }
+    int mid = low+high>>1;
+    build(low,mid,2*node+1);
+    build(mid+1,high,2*node+2);
+    seg[node]=seg[2*node+1]+seg[2*node+2];
 }
-
-int sum(int node, int start, int end, int l, int right){
-    if (l>r)
-        return 0;
-    if (l == start && r == end)
-        return tree[node];
-    int mid = ( start + end ) / 2;
-    return sum(2*node, start, mid, l, min(r,mid)) + sum(2*node + 1, mid + 1, end, max(l,mid+1), r);
-}
-
-
-int updateRangeLazy(int node, int start, int end, int l, int r, int val){
-    if (lazy[node] != 0)
+ 
+void update(int low,int high,int lq,int hq,int node,long long int val)
+{
+    if(lazy[node])
     {
-        tree[node] += (end - start + 1) * lazy[node];
-        if (start != end)
+        seg[node]+=(high-low+1)*lazy[node];
+        if(high!=low)
         {
-            lazy[node*2] += lazy[node];
-            lazy[node*2+1] += lazy[node];
+            lazy[2*node+1]+=lazy[node];
+            lazy[2*node+2]+=lazy[node];
         }
-        lazy[node] = 0;
+        lazy[node]=0;
+    }
+    if(low>hq || high<lq)
+    return;
+    if(lq<=low && high<=hq)
+    {
+        seg[node]+=(high-low+1)*val;
+        if(high!=low)
+        {
+            lazy[2*node+1]+=val;
+            lazy[2*node+2]+=val;
+        }
+        return;
+    }
+    int mid=low+high>>1;
+    update(low,mid,lq,hq,2*node+1,val);
+    update(mid+1,high,lq,hq,2*node+2,val);
+    seg[node]=seg[2*node+1]+seg[2*node+2];
+}
+ 
+long long int query(int low,int high,int lq,int hq,int node)
+{
+    if(lazy[node])
+    {
+        seg[node]+=(high-low+1)*lazy[node];
+        if(high!=low)
+        {
+            lazy[2*node+1]+=lazy[node];
+            lazy[2*node+2]+=lazy[node];
+        }
+        lazy[node]=0;
+    }
+    if(low>hq || high<lq)
+    return 0;
+    if(lq<=low && high<=hq)
+    {
+        return seg[node];
+    }
+    int mid=low+high>>1;
+   return query(low,mid,lq,hq,2*node+1)+query(mid+1,high,lq,hq,2*node+2);
+}
+int main(){
+    int t;
+    scanf("%d",&t);
+    int n,q;
+    while(t--)
+    {
+        memset(seg,0,sizeof(seg));
+        memset(lazy,0,sizeof(lazy));
+        scanf("%d %d",&n,&q);
+        int type;
+        int x,y;
+        long long int val;
+        build(0,n-1,0);
+        while(q--)
+        {
+            scanf("%d",&type);
+            //printf("%dhh\n",type);
+            if(type)
+            {
+                scanf("%d %d",&x,&y);
+                printf("%lld\n",query(0,n-1,x-1,y-1,0));
+            }
+            else
+            {
+                scanf("%d %d %lld",&x,&y,&val);
+                update(0,n-1,x-1,y-1,0,val);
+            }
+        }
          
     }
-    
-    if(start > end || start > r || end < l)
-        return 0;
-    if(start >=  l && end <= r){
-        tree[node] += (end - start + 1) * val;
-        if(start !=  end){
-            lazy[node*2] += val;
-            lazy[node*2+1] += val; 
-        }
-        return tree[node];
-    }
-    int mid = (start + end) / 2;
-    updateRangeLazy(node*2, start, mid, l, r, val);
-    updateRangeLazy(node*2+1, mid+1, end, l, r, val);
-    tree[node] = tree[node*2] + tree[node*2+1];
+     
+    return 0;
+     
 }
