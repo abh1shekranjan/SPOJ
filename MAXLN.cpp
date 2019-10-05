23ef99aadda0ffa3002a1229a0e482b768e30e0a
Abhishek Ranjan <ranjan.abhishek.kotn@gmail.com>
2019-10-05 10:35:35 +0530

    Simple Differentiation

---
+++ MAXLN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    for (int i = 1; i <= t; i++)
+    {
+        long long r;
+        cin>>r;
+        cout<<"Case "<<i<<":"<<" "<<(4*r*r)<<".25"<<endl;
+    }
+    
+}
